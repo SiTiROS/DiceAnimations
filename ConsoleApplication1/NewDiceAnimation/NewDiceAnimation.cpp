c311ef2bfba22304ce6a5eb92bff39ee43381b2a
SiTiROS <sitiros666@gmail.com>
2025-03-18 03:43:08 +0600

    add const correction + minor fix

--- ConsoleApplication1/NewDiceAnimation/NewDiceAnimation.cpp
+++ ConsoleApplication1/NewDiceAnimation/NewDiceAnimation.cpp
@@ -8,29 +8,21 @@
 #include <sstream>
 #include <algorithm>
 
-#ifdef _WIN32
-#define CLEAR_SCREEN "cls"
-#else
-#define CLEAR_SCREEN "clear"
-#endif
-
-
+const int		DICE_QUANTITY = 3;										// Кол-во кубиков
 const int		SIZE_XY = 3;											// Размер кубика (3x3)
-const int		FULLSIZE_X = 9;											// Размер кубика с учётом окантовки (5x9) - Горизонтальная координата
-const int		FULLSIZE_Y = 5;											// Размер кубика с учётом окантовки (5x9) - Вертикальная координата
+const int		FULLSIZE_X = SIZE_XY * 2 + 3;							// Размер кубика с учётом окантовки (5x9) - Горизонтальная координата
+const int		FULLSIZE_Y = SIZE_XY + 2;								// Размер кубика с учётом окантовки (5x9) - Вертикальная координата
 const int		MAX_INDENT_X = 40;										// Ширина рамки экрана
 const int		MAX_INDENT_Y = 20;										// Высота рамки экрана
 const int		FRAME_WIDTH = MAX_INDENT_X + FULLSIZE_X + 1;			// Ширина рамки (с учетом отступов кубика)
 const int		FRAME_HEIGHT = MAX_INDENT_Y + FULLSIZE_Y + 1;			// Высота рамки (с учетом отступов кубика)
-
-const int		diceQuantity = 3;
-const float		gravity = 2;											// Ускорение свободного падения (в пикселях/кадр^2)
-const int		damping = 8;											// Демпфирование при отскоке (делитель на 10, т.е. 0.8)
-const float		friction = 8;											// Трение (делитель на 10, т.е. 0.9)
-const float		stop_threshold = 1.1;									// Порог остановки (если скорость меньше, считаем, что кубик остановился)
-const int		time_step = 6;											// Фиксированный шаг времени для физики (в миллисекундах)
-const int		roll_duration_ms = 1000;								// Общая длительность анимации (в миллисекундах)
-const int		num_frames = roll_duration_ms / time_step;				// Количество кадров анимации
+const int		TIME_STEP = 6;											// Фиксированный шаг времени для физики (в миллисекундах)
+const int		ROLL_DURATION_MS = 1000;								// Общая длительность анимации (в миллисекундах)
+const int		NUM_FRAMES = ROLL_DURATION_MS / TIME_STEP;				// Количество кадров анимации
+const float		GRAVITY = 1.5;											// Ускорение свободного падения (в пикселях/кадр^2)
+const float		DAMPING = 8;											// Демпфирование при отскоке (делитель на 10, т.е. 0.8)
+const float		FRICTION = 7;											// Трение (делитель на 10, т.е. 0.9)
+const float		STOP_THRESHOLD = 0.7;									// Порог остановки (если скорость меньше, считаем, что кубик остановился)
 
 
 // Массив граней кубика. Каждая грань - 3x3 матрица.
@@ -81,7 +73,7 @@ struct Dice
 
 
 // Функция для заполнения массива кубика
-void CreateDice(char(&Dicearray)[FULLSIZE_Y][FULLSIZE_X], int face)
+void CreateDice(char(&Dicearray)[FULLSIZE_Y][FULLSIZE_X], const int face)
 {
 	// +-------+
 	// | * * * |
@@ -126,7 +118,7 @@ void CreateDice(char(&Dicearray)[FULLSIZE_Y][FULLSIZE_X], int face)
 
 
 // Функция для отображения всего игрового поля с кубиком внутри
-void DisplayFrame(Dice dice[], size_t dicesSize)
+void DisplayFrame(const Dice dice[], const size_t dicesSize)
 {
 	std::vector<std::string> frame(FRAME_HEIGHT);	// Создаем вектор строк - заполняем строки сверху вниз горизонтальными линиями
 
@@ -181,23 +173,24 @@ void DisplayFrame(Dice dice[], size_t dicesSize)
 // Обнаружение столкновений
 bool IsColliding(const Dice& a, const Dice& b)
 {
-	return (a.x < b.x + FULLSIZE_X + 1 && a.x + FULLSIZE_X + 1 > b.x &&
-		a.y < b.y + FULLSIZE_Y + 1 && a.y + FULLSIZE_Y + 1 > b.y);
+	return (a.x < b.x + FULLSIZE_X && a.x + FULLSIZE_X > b.x &&
+		a.y < b.y + FULLSIZE_Y && a.y + FULLSIZE_Y > b.y);
 }
 
+
 // Остановка скорости
-void StopSpeed(Dice& dice, int stop_threshold, bool typeSpeed)
+void StopSpeed(Dice& dice, const int STOP_THRESHOLD, const bool typeSpeed)
 {
-	if (typeSpeed = true)
+	if (typeSpeed == true)
 	{
-		if (abs(dice.vx) < stop_threshold)
+		if (abs(dice.vx) <= STOP_THRESHOLD)
 		{
 			dice.vx = 0;
 		}
 	}
 	else
 	{
-		if (abs(dice.vy) < stop_threshold)
+		if (abs(dice.vy) <= STOP_THRESHOLD)
 		{
 			dice.vy = 0;
 		}
@@ -208,8 +201,8 @@ void StopSpeed(Dice& dice, int stop_threshold, bool typeSpeed)
 // Разрешаем коллизии
 void ResolveOverlap(Dice& a, Dice& b)
 {
-	int overlapX = std::min(a.x + FULLSIZE_X, b.x + FULLSIZE_X) - std::max(a.x, b.x);
-	int overlapY = std::min(a.y + FULLSIZE_Y, b.y + FULLSIZE_Y) - std::max(a.y, b.y);
+	int overlapX = std::min(a.x + FULLSIZE_X + 1, b.x + FULLSIZE_X + 1) - std::max(a.x, b.x);
+	int overlapY = std::min(a.y + FULLSIZE_Y + 1, b.y + FULLSIZE_Y + 1) - std::max(a.y, b.y);
 
 	if (overlapX < overlapY) // Смещение по X
 	{
@@ -232,11 +225,11 @@ void ResolveOverlap(Dice& a, Dice& b)
 }
 
 
-// Отскоки у кубиков
+// Изменение скорости у кубиков при столкновении
 void ElasticCollision(Dice& a, Dice& b)
 {
-	float tempVx = 1.01;
-	float tempVy = 0.5;
+	float tempVx = 1.1;
+	float tempVy = 0.8;
 
 	a.vx *= tempVx;
 	a.vy *= tempVy;
@@ -246,47 +239,49 @@ void ElasticCollision(Dice& a, Dice& b)
 
 
 // Отталкивание кубиков от стенок
-void KeepInsideBounds(Dice diceArray[], size_t dicesSize)
+void KeepInsideBounds(Dice diceArray[], const size_t dicesSize)
 {
-	for (size_t currentDice = 0; currentDice < dicesSize; currentDice++)
+	for (size_t i = 0; i < dicesSize; i++)
 	{
 
-		if (diceArray[currentDice].x <= 0)									// Если кубик столкнулся с левой границей
-		{
-			diceArray[currentDice].x = 0;
-			diceArray[currentDice].vx = -diceArray[currentDice].vx * (damping + 2) / 10;
+		Dice& dice = diceArray[i];							// Для краткости кода
 
-			StopSpeed(diceArray[currentDice], stop_threshold, true);
+		if (dice.x <= 0)									// Если кубик столкнулся с левой границей
+		{
+			dice.x = 0;
+			dice.vx = -dice.vx * DAMPING / 10;
+
+			StopSpeed(dice, STOP_THRESHOLD, true);
 		}
-		else if (diceArray[currentDice].x >= MAX_INDENT_X)					// Если кубик столкнулся с правой границей
+		else if (dice.x >= MAX_INDENT_X)					// Если кубик столкнулся с правой границей
 		{
-			diceArray[currentDice].x = MAX_INDENT_X;
-			diceArray[currentDice].vx = -diceArray[currentDice].vx * (damping + 2) / 10;
+			dice.x = MAX_INDENT_X;
+			dice.vx = -dice.vx * DAMPING / 10;
 
-			StopSpeed(diceArray[currentDice], stop_threshold, true);
+			StopSpeed(dice, STOP_THRESHOLD, true);
 		}
 
-		if (diceArray[currentDice].y <= 0)									// Если кубик столкнулся с верхней границей
+		if (dice.y <= 0)									// Если кубик столкнулся с верхней границей
 		{
-			diceArray[currentDice].y = 0;
-			diceArray[currentDice].vy = -diceArray[currentDice].vy * damping / 10;
+			dice.y = 0;
+			dice.vy = -dice.vy * DAMPING / 10;
 
-			StopSpeed(diceArray[currentDice], stop_threshold, false);
+			StopSpeed(dice, STOP_THRESHOLD, false);
 		}
-		else if (diceArray[currentDice].y >= MAX_INDENT_Y)					// Если кубик столкнулся с нижней границей
+		else if (dice.y >= MAX_INDENT_Y)					// Если кубик столкнулся с нижней границей
 		{
-			diceArray[currentDice].y = MAX_INDENT_Y;
-			diceArray[currentDice].vy = -diceArray[currentDice].vy * (damping - 0.1) / 10;
-			diceArray[currentDice].vx = diceArray[currentDice].vx * friction / 10;
+			dice.y = MAX_INDENT_Y;
+			dice.vy = -dice.vy * DAMPING / 10;
+			dice.vx = dice.vx * FRICTION / 10;
 
-			StopSpeed(diceArray[currentDice], stop_threshold, false);
+			StopSpeed(dice, STOP_THRESHOLD, false);
 		}
 	}
 }
 
 
 // Функция для рандома
-int GenerateRandomSize(int i)
+int GenerateRandomSize(const int i)
 {
 	static bool initialized = false;
 	if (!initialized)
@@ -300,7 +295,7 @@ int GenerateRandomSize(int i)
 
 
 // Случайная грань для анимации
-void GenerateDiceFaces(Dice diceArray[], size_t dicesSize)
+void GenerateDiceFaces(Dice diceArray[], const size_t dicesSize)
 {
 	for (size_t i = 0; i < dicesSize; i++)
 	{
@@ -310,29 +305,56 @@ void GenerateDiceFaces(Dice diceArray[], size_t dicesSize)
 
 
 // Проверка на остановку анимаций
-bool StopAnimation(Dice diceArray[], size_t dicesSize)
+bool StopAnimation(const Dice diceArray[], const size_t dicesSize)
 {
 	std::vector<bool> tempSpeeds(dicesSize);
 
 	for (size_t currentDice = 0; currentDice < dicesSize; currentDice++)
 	{
+
+		//std::cout << diceArray[currentDice].face << " VX: " << (abs(diceArray[currentDice].vx)) << std::endl;
+		//std::cout << diceArray[currentDice].face << " VY: " << (abs(diceArray[currentDice].vy)) << std::endl;
+
 		tempSpeeds[currentDice] = false;
-		if (abs(diceArray[currentDice].vx) <= stop_threshold && abs(diceArray[currentDice].vy) <= stop_threshold)
+		if (abs(diceArray[currentDice].vy) <= STOP_THRESHOLD && abs(diceArray[currentDice].vx) <= STOP_THRESHOLD)
 		{
 			tempSpeeds[currentDice] = true;
 		}
 	}
 
+	// у всех кубиков должны быть нулевые скорости
 	return std::all_of(tempSpeeds.begin(), tempSpeeds.end(), [](bool b) { return b; });
 }
 
+
+// Подсчёт финального кол-ва очков выпавших на кубиках
+int CalculateResult(const Dice diceArray[], const size_t dicesSize)
+{
+	int result = 0;
+	for (size_t i = 0; i < dicesSize; i++)
+	{
+		result += diceArray[i].face;
+	}
+	return result;
+}
+
+
+// Чистим консоль
+void СlearScreen()
+{
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
 
 
 int main()
 {
 	// Создание кубиков
-	Dice diceArray[diceQuantity];
-	for (int i = 0; i < diceQuantity; i++)
+	Dice diceArray[DICE_QUANTITY];
+	for (int i = 0; i < DICE_QUANTITY; i++)
 	{
 		diceArray[i] = { GenerateRandomSize(5), GenerateRandomSize(3), 0, 0, i + 1 };
 		diceArray[i].vx = GenerateRandomSize(21) - 10;							// Случайная горизонтальная скорость от -10 до 10
@@ -340,9 +362,9 @@ int main()
 	}
 
 	// После спавна раздвигаем кубики
-	for (size_t i = 0; i < diceQuantity; i++)
+	for (size_t i = 0; i < DICE_QUANTITY; i++)
 	{
-		for (size_t j = i + 1; j < diceQuantity; j++)
+		for (size_t j = i + 1; j < DICE_QUANTITY; j++)
 		{
 			if (IsColliding(diceArray[i], diceArray[j]))
 			{
@@ -352,14 +374,14 @@ int main()
 	}
 
 	// Цикл движения и разрешения столкновений
-	for (int frame = 0; frame < num_frames; ++frame)
+	for (int frame = 0; frame < NUM_FRAMES; ++frame)
 	{
-		system(CLEAR_SCREEN);
+		СlearScreen();
 
-		for (size_t currentDice = 0; currentDice < diceQuantity; currentDice++)
+		for (size_t currentDice = 0; currentDice < DICE_QUANTITY; currentDice++)
 		{
 			// Применяем гравитацию
-			diceArray[currentDice].vy += gravity;													// Увеличиваем вертикальную скорость на величину ускорения свободного падения
+			diceArray[currentDice].vy += GRAVITY;													// Увеличиваем вертикальную скорость на величину ускорения свободного падения
 
 			// Обновляем позицию кубика
 			diceArray[currentDice].x += diceArray[currentDice].vx;									// Изменяем горизонтальную координату
@@ -367,9 +389,9 @@ int main()
 
 
 			// Столкновения кубиков
-			for (size_t i = 0; i < diceQuantity; i++)
+			for (size_t i = 0; i < DICE_QUANTITY; i++)
 			{
-				for (size_t j = i + 1; j < diceQuantity; j++)
+				for (size_t j = i + 1; j < DICE_QUANTITY; j++)
 				{
 					if (IsColliding(diceArray[i], diceArray[j]))
 					{
@@ -380,32 +402,27 @@ int main()
 			}
 
 			// Обработка столкновений с границами
-			KeepInsideBounds(diceArray, diceQuantity);
-
+			KeepInsideBounds(diceArray, DICE_QUANTITY);
 		}
 
 		// Генерим грани у кубика
-		GenerateDiceFaces(diceArray, diceQuantity);
+		GenerateDiceFaces(diceArray, DICE_QUANTITY);
 
 		// Отрисовка текущего кадра
-		DisplayFrame(diceArray, diceQuantity);
+		DisplayFrame(diceArray, DICE_QUANTITY);
 
 
 		// Условие остановки анимации: скорости близки к нулю
-		if (StopAnimation(diceArray, diceQuantity)) break;
+		if (StopAnimation(diceArray, DICE_QUANTITY)) break;
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(time_step));	// Пауза между кадрами
+		std::this_thread::sleep_for(std::chrono::milliseconds(TIME_STEP));	// Пауза между кадрами
 	}
 
-	system(CLEAR_SCREEN);
-	DisplayFrame(diceArray, diceQuantity);									// Отображаем кубики с результатом
+	СlearScreen();
+	DisplayFrame(diceArray, DICE_QUANTITY);									// Отображаем кубики с результатом
 
-	int result = 0;
-	for (size_t i = 0; i < diceQuantity; i++)
-	{
-		result += diceArray[i].face;
-	}
-	std::cout << "Congratulation on the dices fell " << result << " points" << std::endl;
+
+	std::cout << "Congratulation on the dices fell " << CalculateResult(diceArray, DICE_QUANTITY) << " points" << std::endl;
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	return 0;
